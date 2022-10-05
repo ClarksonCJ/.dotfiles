d4e1afd5c345f7cc1e0c7257d18bb6ac276a4ac5
Nekiruy <chrisjclarkson.cjc@gmail.com>
2022-10-05 23:30:21 +0100

    fix path

--- nvim/.config/nvim/lua/clarksoncj/packer.lua
+++ nvim/.config/nvim/lua/clarksoncj/packer.lua
@@ -1,6 +1,6 @@
 local ensure_packer = function()
     local fn = vim.fn
-    local install_path = os.getenv('HOME')..'/.local/share/nvim//site/pack/packer/start/packer.nvim'
+    local install_path = os.getenv('HOME')..'/.local/share/nvim/site/pack/packer/start/packer.nvim'
     if fn.empty(fn.glob(install_path)) > 0 then
         fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
         vim.cmd [[packadd packer.nvim]]
